8198174ce27b729aa84fab913ebaf3b150e2ef4b
Kamil <ksenikeev@mail.ru>
2022-03-09 15:21:15 +0300

    EM3

--- s2_lab4/src/main/java/ru/itis/dis/s2lab4/controllers/DbController.java
+++ s2_lab4/src/main/java/ru/itis/dis/s2lab4/controllers/DbController.java
@@ -3,6 +3,8 @@ package ru.itis.dis.s2lab4.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.itis.dis.s2lab4.orm.EntityManager;
 import ru.itis.dis.s2lab4.orm.EntityManagerFactory;
 
 import java.io.IOException;
@@ -16,6 +18,35 @@ public class DbController {
     @RequestMapping("/")
     public void writer(java.io.Writer writer) throws IOException {
         writer.write(entityManagerFactory.testDbWork());
+    }
+
+
+    @RequestMapping("/add")
+    public void add(java.io.Writer writer) throws IOException {
+        /*
+        User user = new User();
+        user.seId(random);
+        user.setName(...);
+        EntityManager em = entityManagerFactory.getEntityManager();
+        em.save(user);
+
+        entityManagerFactory.closeEntityManager();
+        */
+        writer.write(entityManagerFactory.testDbWork());
+    }
+
+    @RequestMapping("/edit")
+    public void edit(@RequestParam("id") Long id) throws IOException {
+        /*
+        EntityManager em = entityManagerFactory.getEntityManager();
+        User user = em.find(User.class, id);
+
+        user.setName(.....);
+
+        em.merge(user);
+
+        entityManagerFactory.closeEntityManager();
+        */
     }
 
 }
